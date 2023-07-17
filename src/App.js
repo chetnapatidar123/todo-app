1b4e7147e3532377cd7a0611ed30b1c7f74720d9
chetna patidar <chetnapatidar745@gmail.com>
2023-07-17 15:38:49 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './MyComponents/Header'
+import Todos from './MyComponents/Todos'
+import Footer from './MyComponents/Footer'
+import { useState } from 'react'
+import AddTodo from './MyComponents/AddTodo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  let initTodo;
+  if (localStorage.getItem("todos") === null){
+    initTodo = [];
+  } else{
+    initTodo = JSON.parse(localStorage.getItem("todos"))
+  }
+
+  const onDelete = (todo) => {
+console.log("i am on delete", todo)
+
+setTodos(todos.filter((e) => {
+return e!== todo
+}));
+localStorage.setItem("todos", JSON.stringify(todos))
 }
 
-export default App;
+const addTodo = (title,desc) => {
+  console.log("i am adding  todo" , title,desc)
+
+  let sno;
+  if(todos.length == 0){
+    sno = 0;
+  }else{
+   sno = todos[todos.length-1].sno + 1
+
+  }
+  const myTodo = {
+    sno: sno,
+    title: title,
+    desc: desc
+  }
+
+  setTodos([...todos, myTodo]);
+  console.log(myTodo)
+  localStorage.setItem("todos", JSON.stringify(todos))
+  
+}
+
+  const [todos, setTodos] = useState(initTodo)
+
+  return (
+    <div>
+
+      <Header title="My Todo List" searchBar={false }/>
+      <AddTodo addTodo={addTodo}/>
+      <Todos todos = {todos} onDelete = {onDelete}/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
